a65647783f538314d59e94136925dd2438c0f9b1
kiq-caique <96061515+kiq-caique@users.noreply.github.com>
2024-04-22 15:54:45 -0300

    exercício 2 da lista 2

--- atividade_aberta_02.py
+++ atividade_aberta_02.py
@@ -1,3 +1,8 @@
+# 1) Utilizando vetores e comando de repetição, crie um código que receba as notas de 5
+# atividades, que será informada pelo usuário. Em seguida, faça a média das notas e
+# imprima na tela a média e “Aluno aprovado!” caso a média seja superior ou igual a
+# 60,0 e “Aluno reprovado” caso a média seja inferior a 60,0.
+
 import numpy as np
 notas = np.empty(5, dtype=float)
 
@@ -17,3 +22,17 @@ else:
 
 print(notas)
 print(media)
+
+
+# 2) Crie um programa de repetição que receba uma matriz de tamanho 5x5 e imprima a
+# soma de cada linha
+
+import numpy as np
+matriz = [[1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5], [1,2,3,4,5]]
+
+for linha in range(0,5): # linha = 0,1
+    soma = 0
+for coluna in range(0,5): # coluna = 0,1,2
+    soma = soma + matriz[linha][coluna]
+print(soma)
+
